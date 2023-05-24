02dd9415e788cf7c479259fb072386d5ec9abb6a
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2023-05-24 23:53:16 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,17 @@
+package ArrayList;
+import java.util.*;
+public class sort{
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>();
+
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(4);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println(list);
+    }
+}
